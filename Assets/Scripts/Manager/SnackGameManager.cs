c63b585b96a5d87b261f6f7b6f32e6ea2e1012b2
prof-Jefferson <121240091+prof-Jefferson@users.noreply.github.com>
2023-08-25 16:03:59 -0300

    Update SnackGameManager.cs
    Refatoração, organizando o código, sem adicionar nenhum padrão de projeto. Apenas comentários e reorganização da lógica para o entendimento.

--- Assets/Scripts/Manager/SnackGameManager.cs
+++ Assets/Scripts/Manager/SnackGameManager.cs
@@ -6,179 +6,196 @@ using TMPro;
 
 public class SnakeGameManager : MonoBehaviour
 {
-	public GameObject gameOverPanel;
-	public TextMeshProUGUI scoreTextMeshPro;
-	
-	public GameObject blockPrefab;
-	public Color snakeColor = new Color(1f, 1f, 1f, 0.8f); // 80% de opacidade
-	public Color appleColor = new Color(1f, 0f, 0f, 0.8f); // Vermelho com 80% de opacidade
+    // Variáveis públicas para a interface e outros elementos do jogo
+    public GameObject gameOverPanel;
+    public TextMeshProUGUI scoreTextMeshPro;
+    public GameObject blockPrefab;
+    public Color snakeColor = new Color(1f, 1f, 1f, 0.8f); // Cor da cobra
+    public Color appleColor = new Color(1f, 0f, 0f, 0.8f); // Cor da maçã
 
+    // Variáveis para controlar sons
     public AudioSource backgroundMusic;
     public AudioSource appleSound;
 
+    // Variáveis privadas para controle interno do jogo
     private Vector2[,] positions;
-	private int[,] gameMatrix = new int[10, 20];
+    private float startX = -1.596f;
+    private float startY = 0.211f;
+    private float offset = 0.211f;
 
-	private float startX = -1.596f;
-	private float startY = 0.211f;
-	private float offset = 0.211f;
+    private Snake snake;
+    private Vector2Int applePosition;
 
-	private Snake snake;
-	private Vector2Int applePosition;
-	private Dictionary<Vector2Int, GameObject> boardBlocks = new Dictionary<Vector2Int, GameObject>();
+    // Estrutura para manter os blocos do tabuleiro
+    private Dictionary<Vector2Int, GameObject> boardBlocks = new Dictionary<Vector2Int, GameObject>();
 
-	void Start()
-	{
-		InitializeMatrix();
-		InitializeSnake();
-		PlaceApple();
-		StartCoroutine(SnakeMovementRoutine());
-        backgroundMusic.Play();
+    void Start()
+    {
+        // Inicializações
+        InitializeMatrix();
+        InitializeSnake();
+        PlaceApple();
+        StartCoroutine(SnakeMovementRoutine());
+        backgroundMusic.Play();  // Iniciar música de fundo
     }
 
-	void InitializeMatrix()
-	{
-		positions = new Vector2[10, 20];
+    // Inicializa a matriz de posições do jogo
+    void InitializeMatrix()
+    {
+        positions = new Vector2[10, 20];
 
-		for (int x = 0; x < 10; x++)
-		{
-			for (int y = 0; y < 20; y++)
-			{
-				positions[x, y] = new Vector2(startX + x * offset, startY + y * offset);
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 20; y++)
+            {
+                positions[x, y] = new Vector2(startX + x * offset, startY + y * offset);
 
-				GameObject block = Instantiate(blockPrefab, positions[x, y], Quaternion.identity);
-				block.SetActive(false); // Desativado por padr�o.
+                GameObject block = Instantiate(blockPrefab, positions[x, y], Quaternion.identity);
+                block.SetActive(false);  // O bloco é inicialmente invisível
 
-				boardBlocks[new Vector2Int(x, y)] = block;
-			}
-		}
-	}
+                boardBlocks[new Vector2Int(x, y)] = block;
+            }
+        }
+    }
 
-	void InitializeSnake()
-	{
-		snake = new Snake(new Vector2Int(5, 10), new Vector2Int(1, 0));
-	}
+    // Inicializa a cobra no meio do tabuleiro
+    void InitializeSnake()
+    {
+        snake = new Snake(new Vector2Int(5, 10), new Vector2Int(1, 0));
+    }
 
-	void Update()
-	{
-		HandleInput();
+    // Atualiza o jogo a cada quadro
+    void Update()
+    {
+        HandleInput();
 
-		if (snake.segments[0] == applePosition)
-		{
-			snake.Grow(); // Faz a cobrinha crescer.
-			PlaceApple(); // Reposiciona a ma��.
-						  // E qualquer outra l�gica que voc� queira implementar quando a cobrinha come uma ma��.
-		}
-	}
+        // Se a cobra come a maçã
+        if (snake.segments[0] == applePosition)
+        {
+            snake.Grow();
+            PlaceApple();
+        }
+    }
 
-	void HandleInput()
-	{
-		if (Input.GetKeyDown(KeyCode.UpArrow) && snake.direction.y == 0)
-			snake.direction = new Vector2Int(0, 1);
-		if (Input.GetKeyDown(KeyCode.DownArrow) && snake.direction.y == 0)
-			snake.direction = new Vector2Int(0, -1);
-		if (Input.GetKeyDown(KeyCode.LeftArrow) && snake.direction.x == 0)
-			snake.direction = new Vector2Int(-1, 0);
-		if (Input.GetKeyDown(KeyCode.RightArrow) && snake.direction.x == 0)
-			snake.direction = new Vector2Int(1, 0);
-	}
+    // Controla a entrada do usuário
+    void HandleInput()
+    {
+        // Código para mudar a direção da cobra
+        // Observação: a cobra não pode inverter imediatamente a sua direção
+        if (Input.GetKeyDown(KeyCode.UpArrow) && snake.direction.y == 0)
+            snake.direction = new Vector2Int(0, 1);
+        if (Input.GetKeyDown(KeyCode.DownArrow) && snake.direction.y == 0)
+            snake.direction = new Vector2Int(0, -1);
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && snake.direction.x == 0)
+            snake.direction = new Vector2Int(-1, 0);
+        if (Input.GetKeyDown(KeyCode.RightArrow) && snake.direction.x == 0)
+            snake.direction = new Vector2Int(1, 0);
+    }
 
-	IEnumerator SnakeMovementRoutine()
-	{
-		while (true)
-		{
-			Vector2Int newHeadPosition = snake.segments[0] + snake.direction; // Calcula a nova posição da cabeça
-			if (IsPositionInsideBoard(newHeadPosition)) // Verifica se a nova posição está dentro da matriz
-			{
-				snake.Move();
-				CheckCollisions(); // Verifica as colisões antes de atualizar o tabuleiro
-				UpdateGameBoard();
-			}
-			else
-			{
-				GameOver(); // Se a posição estiver fora da matriz, termina o jogo
-			}
+    // Coroutine para mover a cobra
+    IEnumerator SnakeMovementRoutine()
+    {
+        while (true)
+        {
+            Vector2Int newHeadPosition = snake.segments[0] + snake.direction;
 
-			yield return new WaitForSeconds(0.5f); // Velocidade da cobrinha.
-		}
-	}
+            // Se a nova posição é válida, mover a cobra
+            if (IsPositionInsideBoard(newHeadPosition))
+            {
+                snake.Move();
+                CheckCollisions();
+                UpdateGameBoard();
+            }
+            else
+            {
+                GameOver();
+            }
 
-	bool IsPositionInsideBoard(Vector2Int position)
-	{
-		return position.x >= 0 && position.x < 10 && position.y >= 0 && position.y < 20;
-	}
+            yield return new WaitForSeconds(0.5f);  // Delay para o próximo movimento
+        }
+    }
 
-	
-	void CheckCollisions()
-	{
-		Vector2Int headPos = snake.segments[0];
+    // Verifica se a posição está dentro dos limites do tabuleiro
+    bool IsPositionInsideBoard(Vector2Int position)
+    {
+        return position.x >= 0 && position.x < 10 && position.y >= 0 && position.y < 20;
+    }
 
-		// Verifica se a cobrinha tenta sair da matriz
-		if (headPos.x < 0 || headPos.x >= 10 || headPos.y < 0 || headPos.y >= 20)
-		{
-			GameOver();
-			return;
-		}
+    // Verifica se há colisões
+    void CheckCollisions()
+    {
+        Vector2Int headPos = snake.segments[0];
 
-		// Verifica se a cobrinha colide consigo mesma
-		for (int i = 1; i < snake.segments.Count; i++)
-		{
-			if (snake.segments[i] == headPos)
-			{
-				GameOver();
-				return;
-			}
-		}
+        // Verifica se a cobra saiu do tabuleiro
+        if (!IsPositionInsideBoard(headPos))
+        {
+            GameOver();
+            return;
+        }
 
-		// Verifica se a cobrinha comeu a maçã
-		if (headPos == applePosition)
-		{
-			snake.Grow();
-			PlaceApple();
-            appleSound.Play();
-            scoreTextMeshPro.text = "Score: " + snake.score;
-		}
-	}
+        // Verifica se a cobra colidiu consigo mesma
+        for (int i = 1; i < snake.segments.Count; i++)
+        {
+            if (snake.segments[i] == headPos)
+            {
+                GameOver();
+                return;
+            }
+        }
 
-	void UpdateGameBoard()
-	{
-		foreach (var block in boardBlocks.Values)
-		{
-			block.SetActive(false);
-			block.GetComponent<SpriteRenderer>().color = snakeColor; // definindo a cor padr�o para todos os blocos
-		}
+        // Verifica se a cobra comeu a maçã
+        if (headPos == applePosition)
+        {
+            snake.Grow();
+            PlaceApple();
+            appleSound.Play();  // Tocar som da maçã sendo comida
+            scoreTextMeshPro.text = "Score: " + snake.score;  // Atualizar o placar
+        }
+    }
 
-		foreach (Vector2Int pos in snake.segments)
-		{
-			boardBlocks[pos].SetActive(true);
-			boardBlocks[pos].GetComponent<SpriteRenderer>().color = snakeColor;
-		}
+    // Atualiza o tabuleiro com as posições da cobra e da maçã
+    void UpdateGameBoard()
+    {
+        // Desativar todos os blocos e definir a cor padrão
+        foreach (var block in boardBlocks.Values)
+        {
+            block.SetActive(false);
+            block.GetComponent<SpriteRenderer>().color = snakeColor;
+        }
 
-		boardBlocks[applePosition].SetActive(true);
-		boardBlocks[applePosition].GetComponent<SpriteRenderer>().color = appleColor;
-	}
+        // Ativar e colorir os blocos onde a cobra está
+        foreach (Vector2Int pos in snake.segments)
+        {
+            boardBlocks[pos].SetActive(true);
+            boardBlocks[pos].GetComponent<SpriteRenderer>().color = snakeColor;
+        }
 
+        // Ativar e colorir o bloco onde a maçã está
+        boardBlocks[applePosition].SetActive(true);
+        boardBlocks[applePosition].GetComponent<SpriteRenderer>().color = appleColor;
+    }
 
-	void PlaceApple()
-	{
-		do
-		{
-			applePosition = new Vector2Int(Random.Range(0, 10), Random.Range(0, 20));
-		} while (snake.segments.Contains(applePosition));
+    // Posiciona uma nova maçã em uma posição aleatória que não seja ocupada pela cobra
+    void PlaceApple()
+    {
+        do
+        {
+            applePosition = new Vector2Int(Random.Range(0, 10), Random.Range(0, 20));
+        } while (snake.segments.Contains(applePosition));
 
-		boardBlocks[applePosition].SetActive(true);
-	}
+        boardBlocks[applePosition].SetActive(true);
+    }
 
-	public void RestartGame()
-	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Recarrega a cena atual
-	}
+    // Reinicia o jogo
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 
-	void GameOver()
-	{
-		gameOverPanel.SetActive(true);
-		
-		StopAllCoroutines();
-		// TODO: Implemente a l�gica do GameOver aqui. Voc� pode mostrar uma mensagem, reiniciar o jogo, etc.
-	}
+    // Termina o jogo e mostra o painel de Game Over
+    void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+        StopAllCoroutines();  // Para todas as corrotinas para interromper o movimento da cobra
+    }
 }
